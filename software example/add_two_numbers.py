db7f3ad0d1af3e15b564fb703918f9eaeacc4cb2
leo <leo.bravo.rain@gmail.com>
2021-01-02 12:24:46 -0300

    first commit

---
+++ software example/add_two_numbers.py
@@ -0,0 +1,11 @@
+# 1) get first number
+first_number = 10
+
+# 2) get second number
+second_number = 20
+
+# 3) add two numbers
+result = first_number + second_number
+
+# 4) print result
+print("The result is: ", result)
